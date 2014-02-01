59fc9a85727169b057c934755cd0fa3678b59457
Greg <github@afinspect.com>
2014-02-01 12:20:00 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
 	
 	validates :name, presence: true, length: { maximum: 50 }
 	
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+	validates :email, presence: true, 
+										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6 }
-
 	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
